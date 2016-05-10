5368d02e81282883561bc603e5992c5206d47619
Martin Pernica <martin.developer@hotmail.com>
2016-05-10 11:11:44 +0200

    Added Bootstrap2 for Angular2 part of course

---
+++ Bootstrap2/src/app.component.ts
@@ -0,0 +1,8 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "utb-app",
+  template: "<div class=\"row\"><h1>Hello World</h1></div>"
+})
+export class AppComponent { }
---
+++ Bootstrap2/src/app.ts
@@ -0,0 +1,6 @@
+/// <reference path="../typings/main.d.ts" />
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
